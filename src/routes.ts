6ed4b20e31a5b5e0e23d9e022040d827c1732687
Alex Galhardo <alex.vieira@pravaler.com.br>
2023-02-18 15:31:57 -0300

    fix: update root endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,22 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 
 import GetBalanceController from "./domain/account/getBalance/GetBalanceController";
 import PostEventController from "./domain/account/postEvent/PostEventController";
 import ResetStateController from "./domain/account/resetState/ResetStateController";
 
 export default Router()
+    .get("/", (_: Request, res: Response) => {
+        return res.json({
+            success: true,
+            message: {
+                resume: "Alex Galhardo EBANX Software Engineer Technical Test February 2023",
+                email: "aleexgvieira@gmail.com",
+                github: "https://github.com/AlexGalhardo",
+                linkedin: "https://linkedin.com/in/alexgalhardo",
+                sourceCode: "https://github.com/AlexGalhardo/EBANX-Software-Engineer-Technical-Test-February-2023",
+            }
+        })
+    })
     .post("/reset", ResetStateController.handle)
     .post("/event", PostEventController.handle)
     .get("/balance", GetBalanceController.handle);
